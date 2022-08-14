1d713690b8372c2da47c91a521dde7133c87dfde
higginschen(陈千鹤) <higginschen@tencent.com>
2022-08-14 15:10:29 +0800

    :lipstick: 优化hcl拖动条样式

--- src/views/BasicHue.vue
+++ src/views/BasicHue.vue
@@ -59,11 +59,95 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from "vue";
+import { Ref, ref, watch } from "vue";
 import InputSlider from "@/components/input-slider.vue";
 import { PRESETS } from "@/assets/presets";
 import type { presetValueType } from "@/type/type";
 import * as d3 from "d3";
+import { useHCLStore } from "@/stores/HCL";
+
+const hclColor = useHCLStore();
+
+const hC = 100.0;
+const hL = 75.0;
+
+const cH = 340;
+const cL = 80.0;
+
+const lH = 0;
+const lC = 0.0;
+
+const hMin = ref(d3.hcl(hclColor.H[0], hC, hL).formatRgb());
+const h1 = ref(
+  d3
+    .hcl(hclColor.H[0] + (hclColor.H[1] - hclColor.H[0]) / 8, hC, hL)
+    .formatRgb()
+);
+const h2 = ref(
+  d3
+    .hcl(hclColor.H[0] + (hclColor.H[1] - hclColor.H[0]) / 4, hC, hL)
+    .formatRgb()
+);
+const h3 = ref(
+  d3
+    .hcl(hclColor.H[0] + ((hclColor.H[1] - hclColor.H[0]) * 3) / 8, hC, hL)
+    .formatRgb()
+);
+const h4 = ref(
+  d3
+    .hcl(hclColor.H[0] + (hclColor.H[1] - hclColor.H[0]) / 2, hC, hL)
+    .formatRgb()
+);
+const h5 = ref(
+  d3
+    .hcl(hclColor.H[0] + ((hclColor.H[1] - hclColor.H[0]) * 5) / 8, hC, hL)
+    .formatRgb()
+);
+const h6 = ref(
+  d3
+    .hcl(hclColor.H[0] + ((hclColor.H[1] - hclColor.H[0]) * 3) / 4, hC, hL)
+    .formatRgb()
+);
+const h7 = ref(
+  d3
+    .hcl(hclColor.H[0] + ((hclColor.H[1] - hclColor.H[0]) * 7) / 8, hC, hL)
+    .formatRgb()
+);
+const hMax = ref(d3.hcl(hclColor.H[1], hC, hL).formatRgb());
+const cMin = ref(d3.hcl(cH, hclColor.C[0], cL).formatRgb());
+const cMax = ref(d3.hcl(cH, hclColor.C[1], cL).formatRgb());
+const lMin = ref(d3.hcl(lH, lC, hclColor.L[0]).formatRgb());
+const lMax = ref(d3.hcl(lH, lC, hclColor.L[1]).formatRgb());
+
+watch(hclColor, (newValue) => {
+  hMin.value = d3.hcl(newValue.H[0], hC, hL).formatRgb();
+  h1.value = d3
+    .hcl(newValue.H[0] + (newValue.H[1] - newValue.H[0]) / 8, hC, hL)
+    .formatRgb();
+  h2.value = d3
+    .hcl(newValue.H[0] + (newValue.H[1] - newValue.H[0]) / 4, hC, hL)
+    .formatRgb();
+  h3.value = d3
+    .hcl(newValue.H[0] + ((newValue.H[1] - newValue.H[0]) * 3) / 8, hC, hL)
+    .formatRgb();
+  h4.value = d3
+    .hcl(newValue.H[0] + (newValue.H[1] - newValue.H[0]) / 2, hC, hL)
+    .formatRgb();
+  h5.value = d3
+    .hcl(newValue.H[0] + ((newValue.H[1] - newValue.H[0]) * 5) / 8, hC, hL)
+    .formatRgb();
+  h6.value = d3
+    .hcl(newValue.H[0] + ((newValue.H[1] - newValue.H[0]) * 3) / 4, hC, hL)
+    .formatRgb();
+  h7.value = d3
+    .hcl(newValue.H[0] + ((newValue.H[1] - newValue.H[0]) * 7) / 8, hC, hL)
+    .formatRgb();
+  hMax.value = d3.hcl(newValue.H[1], hC, hL).formatRgb();
+  cMin.value = d3.hcl(cH, newValue.C[0], cL).formatRgb();
+  cMax.value = d3.hcl(cH, newValue.C[1], cL).formatRgb();
+  lMin.value = d3.hcl(lH, lC, newValue.L[0]).formatRgb();
+  lMax.value = d3.hcl(lH, lC, newValue.L[1]).formatRgb();
+});
 
 const presetValue: Ref<[presetValueType] | never[]> = ref([]);
 
@@ -169,7 +253,7 @@ const presetOptions = [
 
 const presetHandleChange = (value: [presetValueType]) => {
   const defaultValue = PRESETS[value[0]];
-  console.log(d3.hcl(defaultValue[1], defaultValue[3], defaultValue[5]).rgb())
+  console.log(d3.hcl(defaultValue[1], defaultValue[3], defaultValue[5]).rgb());
   hDefault.value = [defaultValue[0], defaultValue[1]];
   cDefault.value = [defaultValue[2], defaultValue[3]];
   lDefault.value = [defaultValue[4], defaultValue[5]];
@@ -199,20 +283,30 @@ const presetHandleChange = (value: [presetValueType]) => {
 :deep(.slider-block) {
   #h {
     .el-slider__bar {
-      background-image: linear-gradient(to right, #000000, #FFFFFF);
+      background-image: linear-gradient(
+        to right,
+        v-bind(hMin),
+        v-bind(h1),
+        v-bind(h2),
+        v-bind(h3),
+        v-bind(h4),
+        v-bind(h5),
+        v-bind(h6),
+        v-bind(h7),
+        v-bind(hMax)
+      );
     }
   }
 
   #c {
-
+    .el-slider__bar {
+      background-image: linear-gradient(to right, v-bind(cMin), v-bind(cMax));
+    }
   }
 
   #l {
     .el-slider__bar {
-      background-image: linear-gradient(
-        to right,
-        rgba(0, 0, 0, 1),
-        rgba(255, 255, 255, 1));
+      background-image: linear-gradient(to right, v-bind(lMin), v-bind(lMax));
     }
   }
 }
