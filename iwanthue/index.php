90e4934c9d035fd1b022af4234b085d142a50476
higginschen(陈千鹤) <higginschen@tencent.com>
2022-08-16 17:48:26 +0800

    :ambulance: 修改iwanthue的domain

--- iwanthue/index.php
+++ iwanthue/index.php
@@ -299,7 +299,7 @@ $(document).ready(function(){
     updateColorSpace()
     initVisualPalette()
     initPresets(1)
-    document.domain = '.vercel.app';
+    document.domain = 'vercel.app';
 });
 
 // Init the color samples once for all
