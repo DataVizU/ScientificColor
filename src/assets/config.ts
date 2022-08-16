ee87aafb388a8e06fe1ac91cb3a7066a91627c03
higginschen(陈千鹤) <higginschen@tencent.com>
2022-08-16 18:21:21 +0800

    :wrench: 更新iwanthue地址

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,7 +1,8 @@
 const isTestEnv = false;
 
 export const hueUrl = () => {
-  let url = "https://iwanthue.vercel.app";
+  let url =
+    "https://iwanthue-8giqcn01f7f89132-1301838709.ap-shanghai.app.tcloudbase.com/";
   if (isTestEnv) {
     url = "iwanthue_build/index.thml";
   }
