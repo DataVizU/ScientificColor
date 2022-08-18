d2dafd8f6429a08c3de2f7b37d455a42745c650a
higginschen(陈千鹤) <higginschen@tencent.com>
2022-08-18 18:04:26 +0800

    :rocket: 更新部署文件

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -2,7 +2,7 @@ const isTestEnv = false;
 
 export const hueUrl = () => {
   let url =
-    "https://iwanthue-8giqcn01f7f89132-1301838709.ap-shanghai.app.tcloudbase.com/";
+    "https://iwanthue-3gxr4jyd858ecae2-1301838709.ap-shanghai.app.tcloudbase.com/";
   if (isTestEnv) {
     url =
       "http://localhost:63342/build/index.html?_ijt=631ehkv8c783498n1481ls54rr&_ij_reload=RELOAD_ON_SAVE";
