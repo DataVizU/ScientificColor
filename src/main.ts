7d4922e4af586bb939979d924e567455ae59a3ab
chenqianhe <1278095698@qq.com>
2022-08-21 21:53:17 +0800

    :zap: 优化打包大小和资源导入

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-
 import App from "./App.vue";
 import router from "./router";
 
@@ -11,6 +8,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(ElementPlus);
 
 app.mount("#app");
